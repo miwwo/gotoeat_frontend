7a5b8c91f0a2c0b6eec5dc6bddbb6ef8974b577f
Miwwo <saifer.angel@yandex.ru>
2024-05-23 19:12:15 +0300

    Сделала возможность удаления ингредиента

--- src/components/RecipeComponents/RecipeCreateForm.jsx
+++ src/components/RecipeComponents/RecipeCreateForm.jsx
@@ -1,17 +1,18 @@
-import React, {useEffect, useState} from 'react';
-import { TextField, Button, FormControl, InputLabel, Select, MenuItem } from '@material-ui/core';
+import React, { useEffect, useState } from 'react';
+import { TextField, Button, FormControl } from '@material-ui/core';
 import { Autocomplete } from '@material-ui/lab';
-import {listIngredients} from "../../sevices/RecipeService";
-import {useSelector} from "react-redux";
+import {listIngredients} from "../../sevices/RecipeService"
+import { useSelector } from "react-redux";
 import "./RecipeCreateForm.css";
-function RecipeForm (props){
+import { FaMinus } from "react-icons/fa";
+
+const RecipeCreateForm = ({ trigger, setTrigger }) => {
     const { token } = useSelector((state) => state.user);
 
     const [name, setName] = useState('');
     const [description, setDescription] = useState('');
     const [ingredients, setIngredients] = useState([]);
     const [recipeIngredients, setRecipeIngredients] = useState([]);
-
 
     const [error, setError] = useState(null);
     const [formErrors, setFormErrors] = useState({});
@@ -20,17 +21,20 @@ function RecipeForm (props){
     useEffect(() => {
         const fetchIngredients = async () => {
             try {
-                const response = await listIngredients(token)
+                const response = await listIngredients(token);
                 setIngredients(response);
             } catch (error) {
                 setError(error.message);
             } finally {
                 setLoading(false);
             }
-        }
+        };
         fetchIngredients();
-    }, []);
+    }, [token]);
 
+    useEffect(() => {
+     setFormErrors({})
+    }, [recipeIngredients]);
     const handleAddIngredient = () => {
         setRecipeIngredients([...recipeIngredients, { ingredient: null, quantity: '' }]);
     };
@@ -46,41 +50,39 @@ function RecipeForm (props){
         updatedIngredients[index].quantity = value;
         setRecipeIngredients(updatedIngredients);
     };
-
-    /*const handleRemoveIngredient = (index) => {
-        const updatedIngredients = ingredients.filter((_, i) => i !== index);
-        setIngredients(updatedIngredients);
-    };*/
-
+    const handleRemoveIngredient = (index) => {
+        const updatedIngredients = [...recipeIngredients];
+        updatedIngredients.splice(index, 1);
+        setRecipeIngredients(updatedIngredients);
+    };
     const validateInput = () => {
         let isValid = true;
         let errors = {};
 
         if (!name.trim()) {
             isValid = false;
-            errors.name = "Name is required";   /////////////////////////////////
+            errors.name = "Name is required";
         }
 
         if (recipeIngredients.length === 0) {
             isValid = false;
             errors.listIngredients = "Ingredients are required";
-        }else{
+        } else {
             recipeIngredients.forEach((recipeIngredient, index) => {
                 if (!recipeIngredient.ingredient) {
                     isValid = false;
-                    errors.listIngredientsNameEmpty = "Ingredient is required";
+                    errors[`ingredient${index}`] = "Ingredient is required";
                 }
                 if (!recipeIngredient.quantity) {
                     isValid = false;
-                    errors.listIngredientsQuantityEmpty = "Quantity is required";
+                    errors[`quantity${index}`] = "Quantity is required";
                 }
             });
         }
 
         setFormErrors(errors);
-
         return isValid;
-    }
+    };
 
     const handleSubmit = () => {
         if (!validateInput()) {
@@ -97,6 +99,7 @@ function RecipeForm (props){
         console.log(recipe);
         // Здесь отправляете ваш запрос с recipe
     };
+
     if (loading) {
         return <div>Загрузка...</div>;
     }
@@ -104,72 +107,76 @@ function RecipeForm (props){
     if (error) {
         return <div>{error}</div>;
     }
-    return (props.trigger) ? (
-            <div className="popupForm">
-                <div className="popupForm-inner">
-                    <button className="popupForm-close" onClick={() => props.setTrigger(false)}>Закрыть</button>
-                    <div>
-                        <TextField
-                            label="Название"
-                            value={name}
-                            onChange={(e) => setName(e.target.value)}
-                            fullWidth
-                            margin="normal"
-                            error={!!formErrors.name}
-                            helperText={formErrors.name}
-                        />
-                        <TextField
-                            label="Описание"
-                            value={description}
-                            onChange={(e) => setDescription(e.target.value)}
-                            fullWidth
-                            multiline
-                            margin="normal"
-                        />
-                        {recipeIngredients.map((ingredient, index) => (
-                            <div key={index}>
-                                <FormControl fullWidth margin="normal">
-                                    <Autocomplete
-                                        options={ingredients}
-                                        getOptionLabel={(option) => option.name}
-                                        onChange={(event, value) => handleIngredientChange(index, value)}
-                                        renderInput={(params) =>
-                                            <TextField {...params}
-                                                       label="Ингредиент"
-                                                       error={!!formErrors.listIngredientsNameEmpty}
-                                                       helperText={formErrors.listIngredientsNameEmpty} />}
-                                    />
-                                </FormControl>
-                                <TextField
-                                    label="Количество"
-                                    value={ingredient.quantity}
-                                    onChange={(e) => handleQuantityChange(index, e.target.value)}
-                                    fullWidth
-                                    margin="normal"
-                                    error={!!formErrors.listIngredientsNameEmpty}
-                                    helperText={formErrors.listIngredientsNameEmpty}
+
+    return trigger ? (
+        <div className="popupForm">
+            <div className="popupForm-inner">
+                <button className="popupForm-close" onClick={() => setTrigger(false)}>Закрыть</button>
+                <div>
+                    <TextField
+                        label="Название"
+                        value={name}
+                        onChange={(e) => setName(e.target.value)}
+                        fullWidth
+                        margin="normal"
+                        error={!!formErrors.name}
+                        helperText={formErrors.name}
+                    />
+                    <TextField
+                        label="Описание"
+                        value={description}
+                        onChange={(e) => setDescription(e.target.value)}
+                        fullWidth
+                        multiline
+                        margin="normal"
+                    />
+                    {recipeIngredients.map((ingredient, index) => (
+                        <div className="container p-0" key={index} style={{ display: 'flex', alignItems: 'center', marginBottom: '10px' }}>
+                            <FormControl style={{ width: '50%', marginRight: '10px' }}>
+                                <Autocomplete
+                                    options={ingredients}
+                                    getOptionLabel={(option) => option.name}
+                                    onChange={(event, value) => handleIngredientChange(index, value)}
+                                    renderInput={(params) => (
+                                        <TextField
+                                            {...params}
+                                            label="Ингредиент"
+                                            error={!!formErrors[`ingredient${index}`]}
+                                            helperText={formErrors[`ingredient${index}`]}
+                                        />
+                                    )}
                                 />
-                                {/*<Button
-                                    variant="contained"
-                                    color="secondary"
-                                    onClick={() => handleRemoveIngredient(index)}
-                                    style={{ marginLeft: '10px' }}
-                                >
-                                    Удалить
-                                </Button>*/}
-                            </div>
-                        ))}
-                        <Button variant="contained" color="primary" onClick={handleAddIngredient}>
-                            Добавить ингредиент
-                        </Button>
-                        {formErrors.listIngredients && <p>{formErrors.listIngredienst}</p>}
-                        <Button variant="contained" color="primary" onClick={handleSubmit}>
-                            Создать рецепт
-                        </Button>
-                    </div>
+                            </FormControl>
+                            <TextField
+                                value={ingredient.quantity}
+                                onChange={(e) => {
+                                    const value = e.target.value;
+                                    if (/^\d*$/.test(value)) {
+                                        handleQuantityChange(index, value);
+                                    }
+                                }}
+                                style={{ width: '10%', marginRight: '10px', marginTop:'16px'}}
+                                error={!!formErrors[`quantity${index}`]}
+                                helperText={formErrors[`quantity${index}`]}
+                                inputProps={{ pattern: "[0-9]*" }}
+                            />
+                            <Button variant="contained" color="secondary"
+                                    onClick={() => handleRemoveIngredient(index)}>
+                                <FaMinus/>
+                            </Button>
+                        </div>
+                    ))}
+                    <Button variant="contained" color="primary" onClick={handleAddIngredient}>
+                        Добавить ингредиент
+                    </Button>
+                    {formErrors.listIngredients && <p>{formErrors.listIngredients}</p>}
+                    <Button variant="contained" color="primary" onClick={handleSubmit}>
+                        Создать рецепт
+                    </Button>
                 </div>
             </div>
-    ) : "";
-}
+        </div>
+    ) : null;
+};
 
-export default RecipeForm;
+export default RecipeCreateForm;
